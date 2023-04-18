d17eaced3736781bb079128231972eb1ec74a4ac
JastinXyz <jastinlinggar@gmail.com>
2023-04-18 23:04:15 +0700

    hero done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,27 @@
+import Image from "next/image";
+import { FcSearch } from "react-icons/fc";
+import { Outfit } from "next/font/google";
+
+const outfit = Outfit({ weight: ['400'], subsets: ['latin'] });
+
 export default function Hero() {
     return (
-      <>
-        <div class="hero py-16 bg-[#018870] text-white">
-          <div class="hero-content text-center">
-            <div class="max-w-md">
-              <h1 class="text-5xl font-bold">Hello there</h1>
-              <p class="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-              <button class="btn btn-primary">Get Started</button>
+      <div className={outfit.className}>
+        <div className="hero py-16 bg-[#018870] text-white">
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <div className="flex flex-col mx-auto">
+                <div className="flex justify-center">
+                    <Image src={'/img/logo.png'} width={180} height={180} alt="logo" />
+                </div>
+                <div className="relative mt-8">
+                    <FcSearch className="absolute w-[22px] h-[22px] ml-4 my-3" />
+                    <input type="text" placeholder="Cari surah..." className="input input-bordered w-96 rounded-full pl-12 text-black" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </>
+      </div>
     );
 }
