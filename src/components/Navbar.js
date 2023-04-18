184bcd7f56894859018720a7a07c9c63c54b9140
JastinXyz <jastinlinggar@gmail.com>
2023-04-19 00:33:10 +0700

    navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,22 @@
 import { Philosopher } from 'next/font/google';
+import { GoMarkGithub } from 'react-icons/go';
 import Link from 'next/link';
 
-const philosopher = Philosopher({ weight: ['400'], subsets: ['latin'] });
+const philosopher = Philosopher({ weight: ['400', '700'], subsets: ['latin'] });
 
 export default function Navbar() {
     return (
       <>
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-[#018870] text-white">
           <div className="flex-1">
-            <Link href={'/'} className={`${philosopher.className} btn btn-ghost normal-case text-xl`}>
-                <strong>Al Quran Digital</strong>
+            <Link href={'/'} className={`${philosopher.className} ml-5 normal-case text-xl font-bold`}>
+                Al-Quran Digital
             </Link>
           </div>
           <div className="flex-none">
-            <button className="btn btn-square btn-ghost">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-5 h-5 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-                ></path>
-              </svg>
-            </button>
+            <Link href="https://github.com/JastinXyz/quran-digital" target='__blank' className='mr-5'>
+              <GoMarkGithub className='hover:fill-gray-700 transition delay-100 w-5 h-5' />
+            </Link>
           </div>
         </div>
       </>
