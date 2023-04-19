18e2910180d09cd5fff44a249dcd810cd4533e0a
JastinXyz <jastinlinggar@gmail.com>
2023-04-19 12:46:56 +0700

    fix footnote mark when there is an double mark:

--- src/common/functions.js
+++ src/common/functions.js
@@ -1,18 +1,16 @@
 function parseArti(no, str) {
-    let res;
     const regex = /\d+\)/g;
     const matches = str.match(regex);
 
-    if(!matches) return `${no}. ${str}`;
     if (matches) {
         for (let i = 0; i < matches.length; i++) {
             const match = matches[i];
             const superscript = `<span class="mark-footnote">${match}</span>`;
-            res = str.replace(match, superscript);
+            str = str.replace(match, superscript);
         }
     }
 
-    return `${no}. ${res}`;
+    return `${no}. ${str}`;
 }
 
 exports.parseArti = parseArti;
