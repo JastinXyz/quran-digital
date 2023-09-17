bcad1524ce5ce543990dfb53c9a4a500e0ebca5a
JastinXyz <jastinlinggar@gmail.com>
2023-09-17 09:58:55 +0700

    change GoMarkGithub icon with BsGithub

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { philosopher } from '@/common/fonts';
-import { GoMarkGithub } from 'react-icons/go';
+import { BsGithub } from 'react-icons/bs';
 import Link from 'next/link';
 
 export default function Navbar() {
@@ -13,7 +13,7 @@ export default function Navbar() {
           </div>
           <div className="flex-none">
             <Link href="https://github.com/JastinXyz/quran-digital" target='__blank' className='mr-5'>
-              <GoMarkGithub className='hover:fill-gray-700 transition delay-100 w-5 h-5' />
+              <BsGithub className='hover:fill-gray-700 transition delay-100 w-5 h-5' />
             </Link>
           </div>
         </div>
